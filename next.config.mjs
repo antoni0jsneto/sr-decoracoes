91a2f09dd2ca8e14c540c82611c7b9dfcd92d438
Antonio Neto <antonio.silva6@yahoo.com.br>
2026-04-16 11:31:45 -0300

    feat: implement middleware for dynamic Content Security Policy based on environment

--- next.config.mjs
+++ next.config.mjs
@@ -1,84 +1,14 @@
-const isDev = process.env.NODE_ENV !== "production";
-
-const cspDev = `
-  default-src 'self';
-
-  script-src 'self' 'unsafe-inline' 'unsafe-eval'
-    https://www.googletagmanager.com
-    https://www.google-analytics.com
-    https://tagassistant.google.com
-    https://static.cloudflareinsights.com;
-
-  connect-src 'self'
-    https://www.google-analytics.com
-    https://www.googletagmanager.com
-    https://tagassistant.google.com
-    https://www.google.com;
-
-  img-src 'self' data:
-    https://www.google-analytics.com
-    https://www.googletagmanager.com
-    https://www.google.com
-    https://fonts.gstatic.com;
-
-  frame-src
-    https://www.googletagmanager.com
-    https://tagassistant.google.com;
-
-  style-src 'self' 'unsafe-inline'
-    https://fonts.googleapis.com
-    https://www.googletagmanager.com;
-
-  font-src 'self'
-    https://fonts.gstatic.com;
-`;
-
-const cspProd = `
-  default-src 'self';
-
-  script-src 'self' 'unsafe-inline'
-    https://www.googletagmanager.com
-    https://www.google-analytics.com
-    https://static.cloudflareinsights.com;
-
-  connect-src 'self'
-    https://www.google-analytics.com
-    https://www.googletagmanager.com
-    https://www.google.com;
-
-  img-src 'self' data:
-    https://www.google-analytics.com
-    https://www.googletagmanager.com
-    https://www.google.com
-    https://fonts.gstatic.com;
-
-  frame-src
-    https://www.googletagmanager.com;
-
-  style-src 'self' 'unsafe-inline'
-    https://fonts.googleapis.com;
-
-  font-src 'self'
-    https://fonts.gstatic.com;
-`;
-
-const ContentSecurityPolicy = (isDev ? cspDev : cspProd)
-  .replace(/\n/g, "")
-  .replace(/\s{2,}/g, " ");
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
+  reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
       {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy,
-          },
-        ],
+        protocol: "https",
+        hostname: "**",
       },
-    ];
+    ],
   },
 };
 
