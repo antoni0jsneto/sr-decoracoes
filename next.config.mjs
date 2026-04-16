dbe6e8e9327d83eec6e00894cb1782dba763e38c
Antonio Neto <antonio.silva6@yahoo.com.br>
2026-04-16 11:11:40 -0300

    feat: enhance Content Security Policy and add CSS module declaration

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,39 @@
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
-  img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
-  connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com;
+
+  script-src 'self' 'unsafe-inline' 'unsafe-eval'
+    https://www.googletagmanager.com
+    https://www.google-analytics.com
+    https://tagassistant.google.com;
+
+  connect-src 'self'
+    https://www.google-analytics.com
+    https://www.googletagmanager.com
+    https://tagassistant.google.com;
+
+  img-src 'self' data:
+    https://www.google-analytics.com
+    https://www.googletagmanager.com;
+
+  frame-src
+    https://www.googletagmanager.com
+    https://tagassistant.google.com;
+
   style-src 'self' 'unsafe-inline';
   font-src 'self' https://fonts.gstatic.com;
 `;
-
-const securityHeaders = [
-  {
-    key: "Content-Security-Policy",
-    value: ContentSecurityPolicy.replace(/\n/g, ""),
-  },
-];
 
 const nextConfig = {
   async headers() {
     return [
       {
         source: "/(.*)",
-        headers: securityHeaders,
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\n/g, ""),
+          },
+        ],
       },
     ];
   },
