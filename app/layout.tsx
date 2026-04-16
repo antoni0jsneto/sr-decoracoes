62e2e837fd1a8f524cdff1896e4e435b76c879f8
Antonio Neto <antonio.silva6@yahoo.com.br>
2026-04-16 09:54:35 -0300

    feat: add Google Tag Manager integration for improved tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -156,6 +156,17 @@ export default function RootLayout({
     <html lang="pt-BR">
       <head>
         <Script
+          id="gtm-base"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5KMVZ4R8');`,
+          }}
+        />
+        <Script
           id="schema-org"
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }}
@@ -209,6 +220,14 @@ gtag('config', 'AW-17864340115');`,
       <body
         className={`${inter.variable} ${playfair.variable} font-sans antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5KMVZ4R8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
