9531005fdb8211ceabefac76b579b529c726c164
Antonio Neto <antonio.silva6@yahoo.com.br>
2026-04-16 11:23:48 -0300

    fix: update Content Security Policy to include additional sources and improve formatting

--- next.config.mjs
+++ next.config.mjs
@@ -6,23 +6,29 @@ const cspDev = `
   script-src 'self' 'unsafe-inline' 'unsafe-eval'
     https://www.googletagmanager.com
     https://www.google-analytics.com
-    https://tagassistant.google.com;
+    https://tagassistant.google.com
+    https://static.cloudflareinsights.com;
 
   connect-src 'self'
     https://www.google-analytics.com
     https://www.googletagmanager.com
-    https://tagassistant.google.com;
+    https://tagassistant.google.com
+    https://www.google.com;
 
   img-src 'self' data:
     https://www.google-analytics.com
-    https://www.googletagmanager.com;
+    https://www.googletagmanager.com
+    https://www.google.com;
 
   frame-src
     https://www.googletagmanager.com
     https://tagassistant.google.com;
 
-  style-src 'self' 'unsafe-inline';
-  font-src 'self' https://fonts.gstatic.com;
+  style-src 'self' 'unsafe-inline'
+    https://fonts.googleapis.com;
+
+  font-src 'self'
+    https://fonts.gstatic.com;
 `;
 
 const cspProd = `
@@ -30,24 +36,32 @@ const cspProd = `
 
   script-src 'self' 'unsafe-inline'
     https://www.googletagmanager.com
-    https://www.google-analytics.com;
+    https://www.google-analytics.com
+    https://static.cloudflareinsights.com;
 
   connect-src 'self'
     https://www.google-analytics.com
-    https://www.googletagmanager.com;
+    https://www.googletagmanager.com
+    https://www.google.com;
 
   img-src 'self' data:
     https://www.google-analytics.com
-    https://www.googletagmanager.com;
+    https://www.googletagmanager.com
+    https://www.google.com;
 
   frame-src
     https://www.googletagmanager.com;
 
-  style-src 'self' 'unsafe-inline';
-  font-src 'self' https://fonts.gstatic.com;
+  style-src 'self' 'unsafe-inline'
+    https://fonts.googleapis.com;
+
+  font-src 'self'
+    https://fonts.gstatic.com;
 `;
 
-const ContentSecurityPolicy = (isDev ? cspDev : cspProd).replace(/\n/g, "");
+const ContentSecurityPolicy = (isDev ? cspDev : cspProd)
+  .replace(/\n/g, "")
+  .replace(/\s{2,}/g, " ");
 
 const nextConfig = {
   async headers() {
